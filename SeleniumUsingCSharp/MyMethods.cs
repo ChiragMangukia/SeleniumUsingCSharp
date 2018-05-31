a0661c8c9c0ed64c37895af5786c590384444f73
ChiragMangukia <chirag.mangukia@gmail.com>
2018-05-31 21:58:41 +0530

    First Commit

---
+++ SeleniumUsingCSharp/MyMethods.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+using System.Drawing;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Firefox;
+using OpenQA.Selenium.Support.UI;
+
+namespace SeleniumUsingCSharp
+{
+    class MyMethods
+    {
+        public IWebDriver driver;
+
+        public IWebDriver Firefox()
+        {   
+            FirefoxDriverService service = FirefoxDriverService.CreateDefaultService(@"D:\ASP\SeleniumUsingCSharpSolution\SeleniumUsingCSharp\BrowserDrivers\geckodriver-v0.20.1-win64");
+            return driver = new FirefoxDriver(service);
+        }
+
+        public IWebDriver Chrome()
+        {
+            ChromeDriverService service = ChromeDriverService.CreateDefaultService(@"D:\ASP\SeleniumUsingCSharpSolution\SeleniumUsingCSharp\BrowserDrivers\chromedriver_win32");
+            return driver = new ChromeDriver(service);
+        }
+
+        public void TakeScreenshot(IWebDriver driver, string fileName)
+        {
+            Screenshot screenShot = ((ITakesScreenshot)driver).GetScreenshot();
+            screenShot.SaveAsFile(@"D:\ASP\SeleniumUsingCSharpSolution\SeleniumUsingCSharp\Screenshots\" + fileName + ".png", ScreenshotImageFormat.Png);
+        }
+    }
+}
---
+++ SeleniumUsingCSharp/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Firefox;
+using OpenQA.Selenium.Support.UI;
+
+namespace SeleniumUsingCSharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //OpenUsingChrome();
+            OpenUsingFirefox();
+        }
+
+        private static void OpenUsingFirefox()
+        {
+            MyMethods myMethods = new MyMethods();
+            IWebDriver driver = myMethods.Firefox();
+            driver.Navigate().GoToUrl("https://en.wikipedia.org/wiki/Main_Page");
+            Console.WriteLine(driver.Title);
+            Console.WriteLine(driver.Url);
+            //driver.FindElement(By.Id("lst-ib")).SendKeys("Hello World");
+            myMethods.TakeScreenshot(driver, driver.Title);
+            System.Threading.Thread.Sleep(2000);
+            driver.Close();
+        }
+
+        private static void OpenUsingChrome()
+        {
+            MyMethods myMethods = new MyMethods();
+            IWebDriver driver = myMethods.Chrome();
+            driver.Navigate().GoToUrl("https://www.google.com");
+            Console.WriteLine(driver.Title);
+            Console.WriteLine(driver.Url);
+            driver.FindElement(By.Id("lst-ib")).SendKeys("Hello World");
+            System.Threading.Thread.Sleep(2000);
+            driver.Close();
+        }
+    }
+}
